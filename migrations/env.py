2684e0e4d2903db1b4f0919ca96f51eadfb26434
sanjayraj18 <22bsm052@iiitdmj.ac.in>
2026-07-28 23:08:12 +0530

    v0.1-T5: async database layer (SQLAlchemy 2.0, session-per-request, Alembic)

--- migrations/env.py
+++ migrations/env.py
@@ -9,7 +9,6 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from ragx.config import get_settings
 from ragx.db.base import Base
 
-# this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
