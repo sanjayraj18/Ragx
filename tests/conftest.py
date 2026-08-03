71b2e0b26aa14e33cb8adecd751c765f563c577f
sanjayraj18 <22bsm052@iiitdmj.ac.in>
2026-08-03 23:30:53 +0530

    V0.4

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,7 @@ infrastructure; integration-marked tests expect `docker compose up -d`.
 from collections.abc import Iterator
 
 import pytest
+
 from fastapi.testclient import TestClient
 
 from ragx.api.app import create_app
